9febb4035fc2c95d9ccdcdc7cd206d49dd3e11c7
lemaire-luca <luca.lemaire@student.hepl.be>
2024-06-17 22:25:00 +0200

    modifing single page

---
+++ wp-content/themes/portfolio/single.php
@@ -0,0 +1,37 @@
+<?php get_header(); ?>
+<?php if (have_posts()): while (have_posts()): the_post(); ?>
+        <section class="single">
+            <div class="single__title">
+                <a class="back" href="<?= get_field('projet-back', 'option') ?>">Retour</a>
+                <h2><?= get_field('projet-title')?></h2>
+                <p><?= get_field('projet-intro')?></p>
+                <img src="<?= get_field('projet-image')?>" alt="landing page cv" width="20%">
+            </div>
+            <div class="single__details">
+                <div class="single__details__description">
+                    <h3>Description</h3>
+                    <p><?= get_field('projet-description')?></p>
+                </div>
+                <div class="single__details__tools">
+                    <h3>Outils utilisés</h3>
+                    <p><?= get_field('projet-langages')?></p>
+                    <div class="button_profile">
+                        <a href="">Découvrir le site</a>
+                    </div>
+                </div>
+            </div>
+            <div class="single__gallery">
+                <?php if (have_rows('projet-gallery')) :
+                    while (have_rows('projet-gallery')) : the_row();
+                        $image = get_sub_field('projet-gallery-image');
+                        ?>
+                        <img src="<?= $image ?>" alt="photo du site" width="20%">
+                    <?php endwhile;
+                endif;
+                ?>
+            </div>
+            <?php get_template_part('components/more-project'); ?>
+            <?php get_template_part('components/collaboration'); ?>
+        </section>
+<?php endwhile; endif; ?>
+<?php get_footer(); ?>
