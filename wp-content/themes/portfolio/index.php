0bfe8e7d8b807d0508c1d28b2275197a6117841f
lemaire-luca <luca.lemaire@student.hepl.be>
2024-06-18 11:23:53 +0200

    update lang

--- wp-content/themes/portfolio/index.php
+++ wp-content/themes/portfolio/index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <?php if (have_posts()): while (have_posts()): the_post(); ?>
-    <main>
+    <main role="main">
         <section id="title" class="title">
             <div class="title__infos">
                 <div class="title__infos__text">
@@ -19,7 +19,7 @@
                 <h2 role="heading" aria-level="2" class="profile__infos__big">salut,</h2>
                 <h2 role="heading" aria-level="2" class="profile__infos__small">je m'appelle <span itemprop="name" class="green">Luca</span> !</h2>
                 <p class="profile__infos__p">
-                    Je suis un jeune web developer passionné, toujours aux études. Curieux et motivé, je passe mon temps
+                    Je suis un jeune web <span lang="en">developer</span> passionné, toujours aux études. Curieux et motivé, je passe mon temps
                     à explorer les dernières tendances. Avec ma créativité et mon désir constant d'apprendre, je ne
                     cesse de découvrir de nouvelles choses.
                 </p>
