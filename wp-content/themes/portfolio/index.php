3567b955b5fb366f973b578abc44ad0d18b5d30d
lemaire-luca <luca.lemaire@student.hepl.be>
2024-06-18 11:18:09 +0200

    adding microdata

--- wp-content/themes/portfolio/index.php
+++ wp-content/themes/portfolio/index.php
@@ -6,25 +6,25 @@
                 <div class="title__infos__text">
                     <h2><?= get_field('intro-title',false, false); ?></h2>
                 </div>
-                <img src="<?= get_field('intro-img'); ?>" alt="" width="300px">
+                <img src="<?= get_field('intro-img'); ?>" alt="">
             </div>
         </section>
-        <section id="profile" class="profile">
+        <section itemscope itemtype="https://schema.org/Person" id="profile" class="profile">
             <div class="profile__img">
-                <img src="<?= get_field('profile-img'); ?>" alt="" width="300px">
-                <span class="date">10 Octobre 2002</span>
-                <span class="nationality">Belge</span>
+                <img src="<?= get_field('profile-img'); ?>" alt="">
+                <span itemprop="birthDate" class="date">10 Octobre 2002</span>
+                <span itemprop="nationality" class="nationality">Belge</span>
             </div>
             <div class="profile__infos">
                 <h2 role="heading" aria-level="2" class="profile__infos__big">salut,</h2>
-                <h2 role="heading" aria-level="2" class="profile__infos__small">je m'appelle <span class="green">Luca</span> !</h2>
+                <h2 role="heading" aria-level="2" class="profile__infos__small">je m'appelle <span itemprop="name" class="green">Luca</span> !</h2>
                 <p class="profile__infos__p">
                     Je suis un jeune web developer passionné, toujours aux études. Curieux et motivé, je passe mon temps
                     à explorer les dernières tendances. Avec ma créativité et mon désir constant d'apprendre, je ne
                     cesse de découvrir de nouvelles choses.
                 </p>
                 <div class="profile__infos__button">
-                    <a href="">Découvre moi</a>
+                    <a href="<?= get_field('link-me')?>">Découvrez moi</a>
                 </div>
             </div>
         </section>
